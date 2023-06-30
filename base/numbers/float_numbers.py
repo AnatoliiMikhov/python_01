85d34ba776a78b01e56f89dcb21dadaad8750a35
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-07-01 00:19:26 +0200

    added a few files to the project

---
+++ base/numbers/float_numbers.py
@@ -0,0 +1,16 @@
+# There are a few surprise below
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+# warning LOL
+print(0.2 + 0.1)
+# >>> 0.30000000000000004
+
+print(3 * 0.1)
+# >>> 0.30000000000000004
+
+
+universe_age = 14_000_000_000
+print(f"\nThe Universe age ≥ {universe_age}")
