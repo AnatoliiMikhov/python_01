f161f34eadb02adec4b6fabf998c010dbcab17b9
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-07-02 00:08:19 +0200

    how to get elements

---
+++ base/lists/bicycles_list.py
@@ -0,0 +1,12 @@
+# A simple python list
+# python list methods
+bicycles = ["trek", "cannondale", "redline", "specialized"]
+print(bicycles)
+
+# print first element of the list
+first_bicycle = bicycles[0]
+print(f"First bicycle in the list is {first_bicycle.title()}")
+
+# print last element
+last_elem = bicycles[-1]
+print(f"Last bicycle in the list is: {last_elem.title()}")
