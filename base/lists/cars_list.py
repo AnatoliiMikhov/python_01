2207b5517e97450cf49caf40e9bcb2c19d4e458b
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-07-02 00:07:23 +0200

    added cars_list.py

---
+++ base/lists/cars_list.py
@@ -0,0 +1,27 @@
+# There is a python list of european cars
+european_cars = [
+    "bugatti",
+    "citroen",
+    "peugeot",
+    "renault",
+    "volkswagen",
+    "porsche",
+    "mercedes",
+    "aston martin",
+    "mclaren",
+    "jaguar",
+    "rolls royce",
+    "bentley",
+    "alpha romeo",
+    "ferrari",
+]
+
+message = "I wouls like to buy a white"
+
+print(f"{message} {european_cars[0].title()}")
+print(f"{message} {european_cars[1].title()}")
+print(f"{message} {european_cars[2].title()}")
+print(f"{message} {european_cars[3].title()}")
+print(f"{message} {european_cars[4].title()}")
+print(f"{message} {european_cars[5].title()}")
+print(f"{message} {european_cars[-1].title()}")
