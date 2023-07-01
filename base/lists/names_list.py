5c9c495c2895d71c94f0cfcde6cb56ac7ed51dd7
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-07-02 00:07:08 +0200

    added names_list.py

---
+++ base/lists/names_list.py
@@ -0,0 +1,14 @@
+# The list of some my friends' names
+names = ["Maks", "Vasil", "Ruslan", "Volodya"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[-1])
+
+# Greeting message
+greeting_message = "Hello, how are you"
+print(f"{greeting_message} {names[0]}")
+print(f"{greeting_message} {names[1]}")
+print(f"{greeting_message} {names[2]}")
+print(f"{greeting_message} {names[3]}")
