cae80587d3bb39a9930cc956b3831b23d086d802
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-07-23 22:51:30 +0200

    How to iterate a dictionary

---
+++ dictionaries/favorite_numbers.py
@@ -0,0 +1,4 @@
+favorite_numbers = {"john": 12, "jane": 7, "peter": 42, "mary": 13, "sam": 8}
+
+for name, number in favorite_numbers.items():
+    print(f"{name.title()}'s favorite number is: {number} ")
