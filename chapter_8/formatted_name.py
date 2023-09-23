648c7946bdfa92a063b1d9101973239f659d44fa
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-09-23 21:10:32 +0200

    get_formated_name refactoring

--- chapter_8/formatted_name.py
+++ chapter_8/formatted_name.py
@@ -1,7 +1,7 @@
 # a simple function - format name
 
 
-def get_formatted_name(first_name, last_name):
+def get_formatted_name(first_name="john", last_name="smith"):
     """Returns formatted full name"""
     full_name = f"{first_name} {last_name}"
     return full_name.title()
@@ -10,3 +10,9 @@ def get_formatted_name(first_name, last_name):
 # --------------------------------------------------------------------------- #
 my_name = get_formatted_name(first_name="anatolii", last_name="mikhov")
 print("\n", my_name)
+
+wife_name = get_formatted_name("liudmyla", "mikhova")
+print("\n", wife_name)
+
+default_name = get_formatted_name()
+print("\n", default_name)
