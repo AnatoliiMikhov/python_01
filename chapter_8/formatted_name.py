5b420fb486cc0805ec625505fbf936769d7ff15b
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-09-24 23:44:13 +0200

    added an optional second name entry

--- chapter_8/formatted_name.py
+++ chapter_8/formatted_name.py
@@ -1,14 +1,19 @@
 # a simple function - format name
 
 
-def get_formatted_name(first_name="john", last_name="smith"):
+def get_formatted_name(first_name="john", last_name="smith", middle_name=""):
     """Returns formatted full name"""
-    full_name = f"{first_name} {last_name}"
+    if not middle_name:
+        full_name = f"{first_name} {last_name}"
+    else:
+        full_name = full_name = f"{first_name} {middle_name} {last_name}"
     return full_name.title()
 
 
 # --------------------------------------------------------------------------- #
-my_name = get_formatted_name(first_name="anatolii", last_name="mikhov")
+my_name = get_formatted_name(
+    first_name="anatolii", middle_name="wonderful", last_name="mikhov"
+)
 print("\n", my_name)
 
 wife_name = get_formatted_name("liudmyla", "mikhova")
